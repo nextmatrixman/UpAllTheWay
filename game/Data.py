cfb609ac16696b1f7e44ba35a0d3380280d5e069
Di Shen <dshen@avetta.com>
2016-07-20 00:30:33 -0700

    Rearranging variables to be more readable

--- game/Data.py
+++ game/Data.py
@@ -1,15 +1,15 @@
 class Data(object):
   # GAME PARAMETERS
-  currentLevel = 1
-  maxTime = 7200
-  detectDistance = 4
-  dropRate = 60
   platformCount1 = 15
   collectibleTotal1 = 7
   platformCount2 = 20
   collectibleTotal2 = 10
   
   # OTHER PARAMETERS
+  currentLevel = 1
+  maxTime = 7200
+  detectDistance = 4
+  dropRate = 60
   contactDistance = 1
   winningDistance = 1.5
   frameRate = 60
